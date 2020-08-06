dfee4a5a7dcfc1fed897c856e3f2c9807d60f2c2
shivam2911 <mishrashivam2911@gmail.com>
2020-08-06 18:49:25 +0530

    button

--- src/app/pages/report/report.service.ts
+++ src/app/pages/report/report.service.ts
@@ -10,13 +10,17 @@ export class ReportService {
   url = 'http://localhost:4002/user1/graph/result';
   emp='http://localhost:4002/user1';
   tsks='http://localhost:4002/assign/task';
+  tsk="http://localhost:4002/tasks";
 
   constructor(private http: HttpClient) {
   }
   tList(userId){
         return this.http.get<any>(this.tsks+'/'+userId);}
 
-
+  task(taskid){
+    console.log(taskid)
+    return this.http.get<any>(this.tsk+'/'+taskid);
+  }
 
   geti(){
     return this.http.get<any>(this.emp);}

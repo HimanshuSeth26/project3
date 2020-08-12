a804a4919c310bda87e22438da8156c0c18fb885
sawan <kumarsawan127@gmail.com>
2020-08-12 13:14:58 +0530

    make it better

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const cors = require('cors');
 const morgan = require("morgan");
-
-mongoose.connect("mongodb://localhost:27017/MyDb",
-  { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-  console.log('mongodb connected')
-});
+//
+// mongoose.connect("mongodb://localhost:27017/MyDb",
+//   { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+//   console.log('mongodb connected')
+// });
 //MONGO_DATABASE=mongodb+srv://sawan:sawan@cluster0-nxos0.mongodb.net/test?retryWrites=true&w=majority/PM
 // const databaseUrl='mongodb+srv://sawan:sawan@cluster0-nxos0.mongodb.net/test?retryWrites=true&w=majority';
 
@@ -20,23 +20,23 @@ mongoose.connect("mongodb://localhost:27017/MyDb",
 //   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }).then(() => {
 //   console.log('mongodb connected');
 // })
-// const databaseUrl='mongodb+srv://mishra11:2911mishra@mycloustor0.gaadp.mongodb.net/test?retryWrites=true&w=majority';
-// mongoose.set('useCreateIndex', true);
-//
-// mongoose.connect(databaseUrl, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useFindAndModify: false
-// });
-//
-// mongoose.connection.on('connected', () => {
-//   console.log('Connected to Database')
-// });
-// mongoose.connection.on('error', (err) => {
-//   if (err) {
-//     console.log('err', err);
-//   }
-// });
+const databaseUrl='mongodb+srv://mishra11:2911mishra@mycloustor0.gaadp.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.set('useCreateIndex', true);
+
+mongoose.connect(databaseUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to Database')
+});
+mongoose.connection.on('error', (err) => {
+  if (err) {
+    console.log('err', err);
+  }
+});
 require("./model/user"); // require user.js (model)
 require("./model/newEmployee"); // require newEmployee.js (model)
 require("./model/task"); // require newEmployee.js (model)

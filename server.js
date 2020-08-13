12507e981e82b7c0302cbfd725aca27dd0a0643b
HimanshuSeth26 <68278421+HimanshuSeth26@users.noreply.github.com>
2020-08-13 14:20:44 +0530

    make it better

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const cors = require('cors');
 const morgan = require("morgan");
-//
-// const path = require('path');
+
+const path = require('path');
 // mongoose.connect("mongodb://localhost:27017/MyDb",
 //   { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
 //   console.log('mongodb connected')
